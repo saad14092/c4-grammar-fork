ce895b0f33a41397e96acc3eb04700d3bf90a2e1
torsten-systemticks <torsten.mosis@systemticks.de>
2021-02-28 10:28:32 +0100

    Moved Webview functionalit into dedicated class

--- extension/src/c4-dsl-extension.ts
+++ extension/src/c4-dsl-extension.ts
@@ -12,14 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {ExtensionContext, workspace, languages, commands, Uri, window, Range, Position, ViewColumn} from 'vscode'
+import {ExtensionContext, workspace, languages, commands, window, Range, Position } from 'vscode'
 import * as path from 'path';
 import { LanguageClient, LanguageClientOptions, ServerOptions, Trace, Range as LSRange} from 'vscode-languageclient';
 import { C4SemanticTokenProvider, c4Legend } from './c4-semantic-highlight';
-import { getWebViewContent } from './c4-plantuml-preview';
-import * as pako from 'pako';
-import * as fs from 'fs';
-import got from 'got';
+import { C4PlantUMLPreview } from './c4-plantuml-preview';
 
 const CONF_SEMANTIC_HIGHLIGHTING = "c4.language.SemanticHighlighting"
 const CONF_PLANTUML_GENERATOR = "c4.plantuml.generator"
@@ -78,49 +75,11 @@ export function activate(context: ExtensionContext) {
 
     context.subscriptions.push(disposable);
     
-    const panel = window.createWebviewPanel(
-        'PlantUML Preview',
-        'PlantUML Preview',
-        ViewColumn.One,
-        {}
-      );
-
-      /*
-      if(workspace.workspaceFolders) {
-        const onDiskPath = Uri.file(path.join( workspace.workspaceFolders[0].uri.fsPath, 'plantuml-gen', 'issue39_container_Software System.svg'));    
-        const plantUmlSvgSrc = panel.webview.asWebviewUri(onDiskPath);
-        panel.webview.html = getWebViewContent(plantUmlSvgSrc);
-      }
-    */
-
-    const updateWebView = (fn: string) => {
-
-        if(workspace.workspaceFolders) {
-           const pumlFile = Uri.file(path.join( workspace.workspaceFolders[0].uri.fsPath, 'plantuml-gen', fn)).fsPath
-           const svgUri = Uri.file(path.join( workspace.workspaceFolders[0].uri.fsPath, 'plantuml-gen', fn.replace('puml', 'svg')))
-
-          if(needsUpdate(pumlFile, svgUri.fsPath)) {
-            console.log("Create a new svg file")
-            const urlCode = encode(pumlFile)
-            if(urlCode) {
-                toSVG(urlCode).then( (svg: string) => {
-                    fs.writeFileSync(svgUri.fsPath, svg)
-                    const plantUmlSvgSrc = panel.webview.asWebviewUri(svgUri);
-                    panel.webview.html = getWebViewContent(plantUmlSvgSrc);         
-                });            
-           }
-          }
-          else {
-            console.log("Re-use existing svg file")
-            const plantUmlSvgSrc = panel.webview.asWebviewUri(svgUri);
-            panel.webview.html = getWebViewContent(plantUmlSvgSrc);         
-          }           
-        }    
-    }
+    const svgPreviewPanel = new C4PlantUMLPreview()
 
     commands.registerCommand("c4.show.diagram", (uri: string) => {
         if(workspace.workspaceFolders) {
-            updateWebView(uri)
+            svgPreviewPanel.updateWebView(uri)
         }
     });     
 
@@ -130,49 +89,6 @@ export function activate(context: ExtensionContext) {
     return languageClient;
 }
 
-export function encode(puml: string): string | null {
-
-    try {
-        const content = fs.readFileSync(puml, 'utf8')
-        const data = Buffer.from(content, 'utf8') 
-        const compressed = pako.deflate(data, { level: 9 }) 
-        return Buffer.from(compressed)
-          .toString('base64') 
-          .replace(/\+/g, '-').replace(/\//g, '_')    
-      } catch (err) {
-        console.error(err)
-      }
-
-    return null
-}
-
-/**
- * The svg needs to be updated, if it the svg file does not exist at all or the puml file is newer than the svg file
- * @param pumlFile 
- * @param svgFile 
- */
-export function needsUpdate(pumlFile: string, svgFile: string): Boolean {
-
-    return !fs.existsSync(svgFile) || fs.statSync(pumlFile).mtime > fs.statSync(svgFile).mtime
-
-}
-
-export async function toSVG(encoded: string): Promise<string> {
-
-    const response = await got('https://kroki.io/plantuml/svg/'+encoded);
-    return response.body;
-}
-
-/*
-function sendPlantUmlRenderer() {
-
-    const renderConf = workspace.getConfiguration().get(CONF_PLANTUML_GENERATOR)
-
-    const renderer: string = renderConf !== undefined ? renderConf as string: "StructurizrPlantUMLWriter"
-
-    commands.executeCommand("c4.generator.type", renderer)
-}
-*/
 
 export function deactivate() {
     
