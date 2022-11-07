5a1ee6dcad4ccbabe0f4ab2c5d1e287f0965e33c
torsten-systemticks <torsten.mosis@systemticks.de>
2022-11-07 12:40:07 +0000

    Fixed erronous CLI description

--- language-server/src/main/java/de/systemticks/c4dsl/ls/provider/C4CompletionProvider.java
+++ language-server/src/main/java/de/systemticks/c4dsl/ls/provider/C4CompletionProvider.java
@@ -14,6 +14,7 @@ import org.eclipse.lsp4j.Position;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 import com.structurizr.model.Container;
 import com.structurizr.model.Element;
 import com.structurizr.model.SoftwareSystem;
