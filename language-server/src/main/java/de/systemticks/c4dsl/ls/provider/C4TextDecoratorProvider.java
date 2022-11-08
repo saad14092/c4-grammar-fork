b9325493401e1671583e8674453411ab93e3bfc5
torsten-systemticks <torsten.mosis@systemticks.de>
2022-11-08 18:55:10 +0000

    Make TextDecorationProvider use the same tokenizer functions as CompletionProvider

--- language-server/src/main/java/de/systemticks/c4dsl/ls/provider/C4TextDecoratorProvider.java
+++ language-server/src/main/java/de/systemticks/c4dsl/ls/provider/C4TextDecoratorProvider.java
@@ -1,10 +1,9 @@
 package de.systemticks.c4dsl.ls.provider;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.eclipse.lsp4j.Position;
 import org.eclipse.lsp4j.Range;
@@ -13,17 +12,25 @@ import org.slf4j.LoggerFactory;
 
 import com.structurizr.model.Component;
 import com.structurizr.model.Container;
+import com.structurizr.model.ContainerInstance;
+import com.structurizr.model.DeploymentNode;
 import com.structurizr.model.Element;
-import com.structurizr.model.ModelItem;
+import com.structurizr.model.InfrastructureNode;
+import com.structurizr.model.Person;
 import com.structurizr.model.Relationship;
+import com.structurizr.model.SoftwareSystem;
+import com.structurizr.model.SoftwareSystemInstance;
 
 import de.systemticks.c4dsl.ls.model.C4DocumentModel;
 import de.systemticks.c4dsl.ls.model.C4ObjectWithContext;
+import de.systemticks.c4dsl.ls.utils.LineToken;
+import de.systemticks.c4dsl.ls.utils.LineTokenizer;
 import lombok.Data;
 
 public class C4TextDecoratorProvider {
     
     private static final Logger logger = LoggerFactory.getLogger(C4TextDecoratorProvider.class);
+    private LineTokenizer tokenizer = new LineTokenizer();
 
     public List<DecoratorRange> calculateDecorations(C4DocumentModel model) {
 
@@ -39,7 +46,7 @@ public class C4TextDecoratorProvider {
             String line = model.getLineAt(lineNumber(entry.getKey()));
             C4ObjectWithContext<Element> element = entry.getValue();
 
-            result.addAll(calculateDecoratorsForOneElement(element, line, lineNumber(entry.getKey())));
+            result.addAll(calculateDecoratorsForElement(element, line, lineNumber(entry.getKey())));
 
         });
 
@@ -47,134 +54,129 @@ public class C4TextDecoratorProvider {
             String line = model.getLineAt(lineNumber(entry.getKey()));
             Relationship relationShip = entry.getValue().getObject();
 
-            result.addAll(calculateDecoratorsForOneRelationship(relationShip, line, lineNumber(entry.getKey())));
+            result.addAll(calculateDecoratorsForRelationship(relationShip, line, lineNumber(entry.getKey())));
         });
 
         return result;
     }
 
-    public List<DecoratorRange> calculateDecoratorsForOneRelationship(Relationship relationship, String line, int lineNumber) {
-
-        List<DecoratorRange> result = new ArrayList<>();
-
-        int fromIndex = 0;
-
-        PositionAndLength posObject = calculatePositionAndLengthObject(line, relationship.getDescription(), fromIndex);
-        if(posObject.getStartPos() > 0) {
-            result.add(createDecoratorRange("description: ", lineNumber, posObject.getStartPos()));
-            fromIndex = posObject.getFromIndex();
-        }
-
-        posObject = calculatePositionAndLengthObject(line, relationship.getTechnology(), fromIndex);
-        if(posObject.getStartPos() > 0) {
-            result.add(createDecoratorRange("technology: ", lineNumber, posObject.getStartPos()));
-            fromIndex = posObject.getFromIndex();
-        }
-
-        posObject = calculatePositionAndLengthObject(line, getFirstCustomTag(relationship).orElse(null), fromIndex);
-        if(posObject.getStartPos() > 0) {
-            result.add(createDecoratorRange("tags: ", lineNumber, posObject.getStartPos()));
-            fromIndex = posObject.getFromIndex();
-        }
-
-        return result;
+    public List<DecoratorRange> calculateDecoratorsForRelationship(Relationship relationship, String line, int lineNumber) {
+        return decorationsForRelationship(line, lineNumber, RELATIONSHIP_DECORATIONS);
     }
 
-    public List<DecoratorRange> calculateDecoratorsForOneElement(C4ObjectWithContext<Element> elementWithContext, String line, int lineNumber) {
-        
-        List<DecoratorRange> result = new ArrayList<>();
+    List<DecoratorRange> decorationsForRelationship(String line, int lineNumber, String[] decorationLabels) {
 
-        int fromIndex = 0;
-        
-        if(elementWithContext.getIdentifier() != null) {
-            fromIndex = line.indexOf(elementWithContext.getIdentifier()) + elementWithContext.getIdentifier().length();
-        }
+        List<LineToken> tokens = tokenizer.tokenize(line);
+        int firstIndex = (tokens.get(1).getToken().equals("->")) ? 3 : 2;
 
-        Element element = elementWithContext.getObject();
+        return IntStream.range(0, decorationLabels.length)
+            .filter( i -> firstIndex + i < tokens.size() && !tokens.get(firstIndex + i).getToken().equals("{"))
+            .mapToObj( i -> createDecoratorRange(decorationLabels[i], lineNumber, tokens.get(firstIndex + i).getStart()))
+            .collect(Collectors.toList());
 
-        PositionAndLength posObject = calculatePositionAndLengthObject(line, element.getName(), fromIndex);
-        if(posObject.getStartPos() > 0) {
-            result.add(createDecoratorRange("name: ", lineNumber, posObject.getStartPos()));
-            fromIndex = posObject.getFromIndex();
-        }
-        
-        posObject = calculatePositionAndLengthObject(line, element.getDescription(), fromIndex);
-        if(posObject.getStartPos() > 0) {
-            result.add(createDecoratorRange("description: ", lineNumber, posObject.getStartPos()));
-            fromIndex = posObject.getFromIndex();
-        }
-
-        posObject = calculatePositionAndLengthObject(line, getTechnology(element).orElse(null), fromIndex);
-        if(posObject.getStartPos() > 0) {
-            result.add(createDecoratorRange("technology: ", lineNumber, posObject.getStartPos()));
-            fromIndex = posObject.getFromIndex();
-        }
-
-        posObject = calculatePositionAndLengthObject(line, getFirstCustomTag(element).orElse(null), fromIndex);
-        if(posObject.getStartPos() > 0) {
-            result.add(createDecoratorRange("tags: ", lineNumber, posObject.getStartPos()));
-            fromIndex = posObject.getFromIndex();
-        }
-
-        return result;
     }
 
-    private Optional<String> getTechnology(Element element) {
-        if(element instanceof Component) {
-            return Optional.ofNullable(((Component) element).getTechnology());
+    List<DecoratorRange> decorationsForElement(String line, int lineNumber, String[] decorationLabels) {
+
+        List<LineToken> tokens = tokenizer.tokenize(line);
+        int firstIndex = (tokens.get(1).getToken().equals("=")) ? 3 : 1;
+
+        return IntStream.range(0, decorationLabels.length)
+            .filter( i -> firstIndex + i < tokens.size() && !tokens.get(firstIndex + i).getToken().equals("{"))
+            .mapToObj( i -> createDecoratorRange(decorationLabels[i], lineNumber, tokens.get(firstIndex + i).getStart()))
+            .collect(Collectors.toList());
+    }
+
+    private final static String[] PERSON_DECORATIONS = new String[] {"name: ", "description: ", "tags: "};
+    List<DecoratorRange> decorationsForPerson(String line, int lineNumber) {
+        return decorationsForElement(line, lineNumber, PERSON_DECORATIONS);
+    }
+
+    private final static String[] SOFTWARE_SYSTEM_DECORATIONS = new String[] {"name: ", "description: ", "tags: "};
+    List<DecoratorRange> decorationsForSoftwareSystem(String line, int lineNumber) {
+        return decorationsForElement(line, lineNumber, SOFTWARE_SYSTEM_DECORATIONS);
+    }
+
+    private final static String[] CONTAINER_DECORATIONS = new String[] {"name: ", "description: ", "technology: ", "tags: "};
+    List<DecoratorRange> decorationsForContainer(String line, int lineNumber) {
+        return decorationsForElement(line, lineNumber, CONTAINER_DECORATIONS);
+    }
+
+    private final static String[] COMPONENT_DECORATIONS = new String[] {"name: ", "description: ", "technology: ", "tags: "};
+    List<DecoratorRange> decorationsForComponent(String line, int lineNumber) {
+        return decorationsForElement(line, lineNumber, COMPONENT_DECORATIONS);
+    }
+
+    private final static String[] DEPLOYMENT_NODE_DECORATIONS = new String[] {"name: ", "description: ", "technology: ", "tags: ", "instances: "};
+    List<DecoratorRange> decorationsForDeploymentNode(String line, int lineNumber) {
+        return decorationsForElement(line, lineNumber, DEPLOYMENT_NODE_DECORATIONS);
+    }
+
+    private final static String[] INFRASTRUCUTRE_NODE_DECORATIONS = new String[] {"name: ", "description: ", "technology: ", "tags: "};
+    List<DecoratorRange> decorationsForInfrastructureNode(String line, int lineNumber) {
+        return decorationsForElement(line, lineNumber, INFRASTRUCUTRE_NODE_DECORATIONS);
+    }
+
+    private final static String[] SOFTWARE_SYSTEM_INSTANCE_DECORATIONS = new String[] {"identifier: ", "deploymentGroups: ", "tags: "};
+    List<DecoratorRange> decorationsForSoftwareSystemInstance(String line, int lineNumber) {
+        return decorationsForElement(line, lineNumber, SOFTWARE_SYSTEM_INSTANCE_DECORATIONS);
+    }
+
+    private final static String[] CONTAINER_INSTANCE_DECORATIONS = new String[] {"identifier: ", "deploymentGroups: ", "tags: "};
+    List<DecoratorRange> decorationsForContainerInstance(String line, int lineNumber) {
+        return decorationsForElement(line, lineNumber, CONTAINER_INSTANCE_DECORATIONS);
+    }
+
+    private final static String[] RELATIONSHIP_DECORATIONS = new String[] {"description: ", "technology: ", "tags: "};
+
+    public List<DecoratorRange> calculateDecoratorsForElement(C4ObjectWithContext<Element> elementWithContext, String line, int lineNumber) {
+        
+        List<DecoratorRange> decorations = new ArrayList<>();
+
+        Element context = elementWithContext.getObject();
+        
+        if(context instanceof Person) {
+            decorations = decorationsForPerson(line, lineNumber);
         }
-        else if(element instanceof Container) {
-            return Optional.ofNullable(((Container) element).getTechnology());
+        else if(context instanceof SoftwareSystem) {
+            decorations = decorationsForSoftwareSystem(line, lineNumber);
         }
-        return Optional.empty();
+
+        else if(context instanceof Container) {
+            decorations = decorationsForContainer(line, lineNumber);
+        }
+
+        else if(context instanceof Component) {
+            decorations = decorationsForComponent(line, lineNumber);
+        }
+
+        else if(context instanceof DeploymentNode) {
+            decorations = decorationsForDeploymentNode(line, lineNumber);
+        }
+
+        else if(context instanceof InfrastructureNode) {
+            decorations = decorationsForInfrastructureNode(line, lineNumber);
+        }
+
+        else if(context instanceof SoftwareSystemInstance) {
+            decorations = decorationsForSoftwareSystemInstance(line, lineNumber);
+        }
+
+        else if(context instanceof ContainerInstance) {
+            decorations = decorationsForContainerInstance(line, lineNumber);
+        }
+
+        return decorations;
     }
      
-    Optional<String> getFirstCustomTag(ModelItem element) {
-
-        if(Objects.isNull(element.getDefaultTags())) {
-            return Optional.empty();
-        }
-
-        return Arrays.asList(element.getTags().split(",")).stream()
-            .map(String::trim)
-            .filter( tag -> !element.getDefaultTags().contains(tag))
-            .findFirst();            
-
-    }
-
     private DecoratorRange createDecoratorRange(String type, int line, int character) {
        return new DecoratorRange(type ,new Range(new Position(line, character), new Position(line, character)));
-    }
-
-    PositionAndLength calculatePositionAndLengthObject(String line, String decoratable, int fromIndex) {        
-        if(decoratable == null || decoratable.length() == 0) {
-            return new PositionAndLength(-1, fromIndex);
-        }
-        int startPos = line.indexOf(decoratable, fromIndex);
-        if(startPos > -1 && line.charAt(startPos-1) == '"') {
-            startPos -= 1;
-        }
-
-        return new PositionAndLength(startPos, startPos + decoratable.length());
     }
 
     private int lineNumber(Integer key) {
         return key-1;
     }
-
-    @Data
-    class PositionAndLength {
-
-        private int startPos;
-        private int fromIndex;        
-
-        public PositionAndLength(int startPos, int fromIndex) {
-            this.startPos = startPos;
-            this.fromIndex = fromIndex;
-        }
-        
-    }
-
+ 
     @Data
     public class DecoratorRange {
         
--- language-server/src/test/java/de/systemticks/c4dsl/ls/provider/C4TextDecoratorProviderTest.java
+++ language-server/src/test/java/de/systemticks/c4dsl/ls/provider/C4TextDecoratorProviderTest.java
@@ -1,88 +1,77 @@
 package de.systemticks.c4dsl.ls.provider;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.eclipse.lsp4j.Position;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.structurizr.model.Container;
-import com.structurizr.model.Element;
-
-import de.systemticks.c4dsl.ls.helper.C4TestHelper;
-import de.systemticks.c4dsl.ls.model.C4DocumentModel;
 import de.systemticks.c4dsl.ls.provider.C4TextDecoratorProvider.DecoratorRange;
-import de.systemticks.c4dsl.ls.provider.C4TextDecoratorProvider.PositionAndLength;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class C4TextDecoratorProviderTest {
     
-    private static C4DocumentModel model;
     private static C4TextDecoratorProvider decoratorProvider;
 
-    @BeforeAll
-    public static void initialize() throws IOException, URISyntaxException {
-        model = C4TestHelper.createDocumentFromFile(new File(C4TestHelper.PATH_VALID_MODELS + File.separator + "amazon_web_service.dsl"));
+    @BeforeEach
+    public void setup() {
         decoratorProvider = new C4TextDecoratorProvider();
     }
 
     @Test
-    public void decorationsForNameCalculated() {
-        List<DecoratorRange> ranges = decoratorProvider.calculateDecorations(model);
-        assertTrue(!ranges.isEmpty());
-        System.err.println("Range: "+ranges.get(0));
+    void componentWithoutIdentifier() {
+        String line = "component myname mydescription mytechnology mytags";
+        List<DecoratorRange> decorations = decoratorProvider.decorationsForComponent(line, 0);
+        assertThat(decorations).hasSize(4);
+        assertThat(decorations.stream().map(DecoratorRange::getType)).containsExactly("name: ", "description: ", "technology: ", "tags: ");
+        assertThat(decorations.stream().map(dr -> dr.getRange().getStart().getLine())).allMatch(l -> l == 0);
+        assertThat(decorations.get(0).getRange().getStart()).isEqualTo(new Position(0, 10));
+        assertThat(decorations.get(1).getRange().getStart()).isEqualTo(new Position(0, 17));
+        assertThat(decorations.get(2).getRange().getStart()).isEqualTo(new Position(0, 31));
+        assertThat(decorations.get(3).getRange().getStart()).isEqualTo(new Position(0, 44));
     }
 
     @Test
-    public void oneCustomTag() {
-        Element element = mock(Element.class);
-        when(element.getTags()).thenReturn("Default1, Default2, Custom1");
-        when(element.getDefaultTags()).thenReturn(new HashSet<>(Arrays.asList("Default1", "Default2")));
-        Optional<String> firstCustom = decoratorProvider.getFirstCustomTag(element);
-        assertTrue(firstCustom.isPresent());
-        assertEquals("Custom1", firstCustom.get());
+    void componentWithIdentifier() {
+        String line = "identifier = component myname mydescription mytechnology mytags";
+        List<DecoratorRange> decorations = decoratorProvider.decorationsForComponent(line, 0);
+        assertThat(decorations).hasSize(4);
+        assertThat(decorations.stream().map(DecoratorRange::getType)).containsExactly("name: ", "description: ", "technology: ", "tags: ");
+        assertThat(decorations.stream().map(dr -> dr.getRange().getStart().getLine())).allMatch(l -> l == 0);
     }
 
     @Test
-    public void twoCustomTags() {
-        Element element = mock(Element.class);
-        when(element.getTags()).thenReturn("Default1, Default2, Custom1, Custom2");
-        when(element.getDefaultTags()).thenReturn(new HashSet<>(Arrays.asList("Default1", "Default2")));
-        Optional<String> firstCustom = decoratorProvider.getFirstCustomTag(element);
-        assertTrue(firstCustom.isPresent());
-        assertEquals("Custom1", firstCustom.get());
+    void componentWithIdentifierAndBracket() {
+        String line = "identifier = component myname mydescription mytechnology mytags {";
+        List<DecoratorRange> decorations = decoratorProvider.decorationsForComponent(line, 0);
+        assertThat(decorations).hasSize(4);
+        assertThat(decorations.stream().map(DecoratorRange::getType)).containsExactly("name: ", "description: ", "technology: ", "tags: ");
+        assertThat(decorations.stream().map(dr -> dr.getRange().getStart().getLine())).allMatch(l -> l == 0);
     }
 
     @Test
-    public void noCustomTags() {
-        Element element = mock(Element.class);
-        when(element.getTags()).thenReturn("Default1, Default2");
-        when(element.getDefaultTags()).thenReturn(new HashSet<>(Arrays.asList("Default1", "Default2")));
-        Optional<String> firstCustom = decoratorProvider.getFirstCustomTag(element);
-        assertFalse(firstCustom.isPresent());
+    void componentOnlyName() {
+        String line = "identifier = component myname";
+        List<DecoratorRange> decorations = decoratorProvider.decorationsForComponent(line, 0);
+        assertThat(decorations).hasSize(1);
+        assertThat(decorations.stream().map(DecoratorRange::getType)).containsExactly("name: ");
+        assertThat(decorations.stream().map(dr -> dr.getRange().getStart().getLine())).allMatch(l -> l == 0);
     }
 
     @Test
-    public void positionAndLengthContainerName() {
-        final String LINE = "id = container \"Container Name\" \"Fancy Description\" \"Cool Technology\" {";
-        Container container = mock(Container.class);
-        when(container.getName()).thenReturn("Container Name");
-        PositionAndLength posAndLength = decoratorProvider.calculatePositionAndLengthObject(LINE, container.getName(), 0);
-        assertEquals(15, posAndLength.getStartPos());
+    void personWithQuotedDescription() {
+        String line = "user = person myname \"mydescription\" \"tag1 tag2\"";
+        List<DecoratorRange> decorations = decoratorProvider.decorationsForPerson(line, 0);
+        assertThat(decorations).hasSize(3);
+        assertThat(decorations.stream().map(DecoratorRange::getType)).containsExactly("name: ", "description: ", "tags: ");
+    }
 
-        when(container.getTechnology()).thenReturn("Cool Technology");
-        posAndLength = decoratorProvider.calculatePositionAndLengthObject(LINE, ((Container)container).getTechnology(), 40);
-        assertEquals(52, posAndLength.getStartPos());
+    @Test
+    void softwareSystemDecorations() {
+        String line = "identifier = softwareSystem myname \"mydescription\"  \"mytags\" {";
+        List<DecoratorRange> decorations = decoratorProvider.decorationsForSoftwareSystem(line, 0);
+        assertThat(decorations).hasSize(3);
+        assertThat(decorations.stream().map(DecoratorRange::getType)).containsExactly("name: ", "description: ", "tags: ");
     }
 
 }
