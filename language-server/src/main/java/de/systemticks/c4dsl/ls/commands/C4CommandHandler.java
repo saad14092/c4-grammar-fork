5c14cb1d8eb801be141b0aadb41447f4750e3dc1
torsten-systemticks <torsten.mosis@systemticks.de>
2023-02-23 15:17:15 +0000

    Initial commit for using language-server as submodule

--- language-server/src/main/java/de/systemticks/c4dsl/ls/commands/C4CommandHandler.java
+++
@@ -1,9 +0,0 @@
-package de.systemticks.c4dsl.ls.commands;
-
-import java.util.List;
-
-public interface C4CommandHandler {
-    
-    public C4ExecuteCommandResult handleRequest(List<Object> arguments);
-
-}
